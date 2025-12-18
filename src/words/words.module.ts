0b0b8a8019dea865c2d31f74142b7199fd7841cf
sebastiancifuentes <sebastianc420@gmail.com>
2025-12-18 10:45:54 -0500

    feat: add passportmodule to wordsmodule and change a message

--- src/words/words.module.ts
+++ src/words/words.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { WordsController } from './words.controller';
 import { WordsService } from './words.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Word } from './entities/word.entity';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   controllers: [WordsController],
-  providers: [WordsService]
+  providers: [WordsService],
+  imports: [
+    ConfigModule,
+    TypeOrmModule.forFeature([ Word ]),
+    PassportModule.register({defaultStrategy: 'jwt'}),
+  ],
+  exports: [TypeOrmModule, PassportModule]
 })
 export class WordsModule {}
--- src/words/words.service.ts
+++ src/words/words.service.ts
@@ -38,7 +38,7 @@ export class WordsService {
     async create(word: WordDto) {
         try {
             const exists = await this.repository.findOne({where: { name: word.name }})
-            if (exists) throw new BadRequestException('The word already existed');
+            if (exists) throw new BadRequestException('This word already existed');
 
             const newWord = this.repository.create({
                 ...word
@@ -47,6 +47,7 @@ export class WordsService {
             await this.repository.save(newWord);
 
             return {
+                word: newWord,
                 message: 'Word created succesfully'
             }
 
@@ -74,6 +75,7 @@ export class WordsService {
             await this.repository.save(updatedWord);
 
             return {
+                word: updatedWord,
                 message: 'Word was updated succesfully'
             }
 
